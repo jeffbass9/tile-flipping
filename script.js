9f114a221dab220a8c8d5dd47b14910ba7cd7466
Jeff Bass <jeffbass9@gmail.com>
2017-09-13 14:39:42 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+//The following surrounding function is so that the $ will work, as you usually have to sub in the word "$" to get custom jQuery code to work with Wordpress
+(function($){
+
+//Keep all blog category posts hidden when the page is first loaded
+$(document).ready(function(){
+  $(".blog-category-1").hide();
+  $(".blog-category-2").hide();
+  $(".blog-category-3").hide();
+});
+
+//Hide other categories when category tiles are clicked and corresponding dropdowns are revealed
+$(".dropdown-tile-1").click(function(){
+  $(".blog-category-1").slideToggle("fast");
+  $(".blog-category-2").hide();
+  $(".blog-category-3").hide();
+});
+$(".dropdown-tile-2").click(function(){
+  $(".blog-category-2").slideToggle("fast");
+  $(".blog-category-1").hide();
+  $(".blog-category-3").hide();
+});
+$(".dropdown-tile-3").click(function(){
+  $(".blog-category-3").slideToggle("fast");
+  $(".blog-category-1").hide();
+  $(".blog-category-2").hide();
+});
+
+//Viewport specific functionality
+var notMobile = window.matchMedia("(min-width: 789px)");
+if(notMobile.matches){
+      (function() {
+      var cards = $(".flipping-tile");
+      var length = cards.length;
+      for (var i  = 0; i < length; i++) {
+        var card = cards[i];
+        clickListener(card);
+      }
+      //original function
+      function clickListener(card) {
+        card.addEventListener( "click", function() {
+          var classes = this.classList;
+          classes.contains("flipped") === true ? classes.remove("flipped") : classes.add("flipped");
+          $(this).find("h3").toggle();
+        });
+      }
+    })();
+
+    //Home page dropdown blog post functionality:
+    $('.dropdown-tile').click(function(){
+      //If the tile has been clicked once, turn the class indicating so off, and return all tiles to default state
+      if($(this).hasClass('clicked-once')){
+        $(this).removeClass('clicked-once');
+        $('.dropdown-tile').each(function(){
+          $(this).find('img').removeClass('desaturate');
+          $(this).find('h2').removeClass('darken-text');
+        });
+      }
+
+      //If the tile has not been clicked an odd number of times, add the class that indicates it has now, remove the darkening classes
+      //from the current tile, and give the other tiles darkening classes
+      else{
+        $(this).addClass('clicked-once');
+        $(this).find('img').removeClass('desaturate');
+        $(this).find('h2').removeClass('darken-text');
+
+        $('.dropdown-tile').not(this).each(function(){
+            $(this).removeClass('clicked-once');
+            $(this).find('img').addClass('desaturate');
+            $(this).find('h2').addClass('darken-text');
+        });
+      }
+    });
+}
+else{
+      //Enable the blog categories to dropdown below the tile on mobile views
+      $('.blog-category-1').insertAfter('.dropdown-tile-1');
+      $('.blog-category-2').insertAfter('.dropdown-tile-2');
+      $('.blog-category-3').insertAfter('.dropdown-tile-3');
+
+      //Hide the navigation menu and hid the flipping tile content on mobile views
+      $(".menu").hide();
+      $(".flipping-tile-content").hide();
+
+      //Enable the dropdown functionality on all tiles that flip over on larger views
+      $(".flipping-tile").click(function(){
+        $(this).find(".flipping-tile-content").slideToggle("fast");
+      });
+}
+//End jQuery - Wordpress compatibility function
+})(jQuery );
